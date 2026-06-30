7764f04ab12b0cafc6f0877d23a671066564d376
theta <tom-vautray@hotmail.fr>
2026-06-30 18:08:02 +0200

    feat: add nix run apps for scenario-runner and spectator-client

--- spectator-client/flake.nix
+++ spectator-client/flake.nix
@@ -4,30 +4,51 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "clippy" "rustfmt" ];
+        };
       in
       {
         devShells.default = pkgs.mkShell {
           name = "stargem-spectator";
           nativeBuildInputs = with pkgs; [
-            rustPackages.rustc rustPackages.cargo
-            rustPackages.clippy rustPackages.rustfmt
-            pkg-config
+            rustToolchain pkg-config protobuf
           ];
           buildInputs = with pkgs; [
             alsa-lib udev vulkan-loader
             libxkbcommon wayland
             libx11 libxcursor libxi libxrandr
-            protobuf
           ];
           LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib:${pkgs.libxkbcommon}/lib:${pkgs.wayland}/lib";
-          PROTOC = "${pkgs.protobuf}/bin/protoc";
-          PROTOC_INCLUDE = "${pkgs.protobuf}/include";
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
+            name = "spectator-client";
+            runtimeInputs = with pkgs; [
+              rustToolchain pkg-config protobuf
+              alsa-lib udev vulkan-loader
+              libxkbcommon wayland
+              libx11 libxcursor libxi libxrandr
+            ];
+            text = ''
+              export PROTO_SRC="${self}/protos"
+              export PROTOC="${pkgs.protobuf}/bin/protoc"
+              export PROTOC_INCLUDE="${pkgs.protobuf}/include"
+              export LD_LIBRARY_PATH="${pkgs.vulkan-loader}/lib:${pkgs.libxkbcommon}/lib:${pkgs.wayland}/lib"
+              cargo run --manifest-path "${self}/spectator-client/Cargo.toml" --release -- "$@"
+            '';
+          };
         };
       });
 }
