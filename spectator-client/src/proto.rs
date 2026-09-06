6d77d385ca8de3a5e540f27882a77d2badf5f59c
theta <tom-vautray@hotmail.fr>
2026-09-06 22:52:02 +0200

    style drop code

--- spectator-client/src/proto.rs
+++
@@ -1,3 +0,0 @@
-pub mod common { tonic::include_proto!("stargem.quic.common"); }
-pub mod combat { tonic::include_proto!("stargem.quic.combat"); }
-pub mod spectator { tonic::include_proto!("stargem.grpc.spectator"); }
